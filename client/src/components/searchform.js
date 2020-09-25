c6c1d6251d83c205253a4e0d12d1b6eb83666780
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-09-24 23:47:00 -0400

    Form grabs the start and end date for api queries

--- client/src/components/searchform.js
+++ client/src/components/searchform.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 
 
 const SearchForm =()=>{
 
+const [startDate, setStartDate]= useState('')
+const [endDate, setEndDate]= useState('')
+
+const Data = ()=>{
+  console.log(`Start date ${startDate}`)
+  console.log(`End date ${endDate}`)
+
+}
+
     return(
         <Form>
 <Form.Group >
     <Form.Label>Start Date</Form.Label>
-    <Form.Control  placeholder="Starting date for asteroid search YYYY-MM-DD" />
+    <Form.Control  placeholder="Starting date for asteroid search YYYY-MM-DD" value={startDate} onChange={e => setStartDate(e.target.value) } />
   </Form.Group>
 <Form.Group >
     <Form.Label>Start Date</Form.Label>
-    <Form.Control  placeholder="Ending date for asteroid search YYYY-MM-DD" />
+    <Form.Control  placeholder="Ending date for asteroid search YYYY-MM-DD" value={endDate} onChange={e => setEndDate(e.target.value) }/>
   </Form.Group>
 
-  <Button variant="primary" type="submit">
+  <Button variant="primary" onClick={Data}>
     Submit
   </Button>
         </Form>
