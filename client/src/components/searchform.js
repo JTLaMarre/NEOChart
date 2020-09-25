40a53edb583ba93370de01c1ac7fb8a312c2b726
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-09-25 08:09:57 -0400

    form retrieves data using axios call to api

--- client/src/components/searchform.js
+++ client/src/components/searchform.js
@@ -3,18 +3,44 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import axios from 'axios';
 
+
+const axios = require('axios');
 
 
 const SearchForm =()=>{
 
-const [startDate, setStartDate]= useState('')
-const [endDate, setEndDate]= useState('')
+const [startDate, setStartDate]= useState('');
+const [endDate, setEndDate]= useState('');
+const [data,setData]=useState('')
+
+const handleRequest = ()=>{
+
+  // Make a request for a user with a given ID
+  axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=gqS8iiSxwjYYj5iPfuaEM2hx0mlwK6TVmxNOdtvu`)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+    console.log(`Data is ${data}`)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+    console.log(`request sent`)
+  });
+}
+
+
+
+
 
 const Data = ()=>{
   console.log(`Start date ${startDate}`)
   console.log(`End date ${endDate}`)
+  handleRequest()
 
 }
 
@@ -27,7 +53,7 @@ const Data = ()=>{
     <Form.Control  placeholder="Starting date for asteroid search YYYY-MM-DD" value={startDate} onChange={e => setStartDate(e.target.value) } />
   </Form.Group>
 <Form.Group >
-    <Form.Label>Start Date</Form.Label>
+    <Form.Label>End Date limit of 7 day date range</Form.Label>
     <Form.Control  placeholder="Ending date for asteroid search YYYY-MM-DD" value={endDate} onChange={e => setEndDate(e.target.value) }/>
   </Form.Group>
 
